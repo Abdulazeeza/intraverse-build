a287b729adf70dfb16ddb217c7f64edd1bb4ba3c
unknown <abdulazeeza180@gmail.com>
2020-08-21 01:40:37 +0100

    initial commit

---
+++ precache-manifest.60f2ead1a0933eb50ffdcb1ed60c9b4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323321df3cbecba7ed58a09dcd87ad8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e3800ec057c6a98a08b",
+    "url": "/static/css/main.f40c6bba.chunk.css"
+  },
+  {
+    "revision": "f527749128394696be50",
+    "url": "/static/js/2.eb0962c6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb0962c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e3800ec057c6a98a08b",
+    "url": "/static/js/main.97087af3.chunk.js"
+  },
+  {
+    "revision": "76151430aa213677ecbc",
+    "url": "/static/js/runtime-main.2b220889.js"
+  }
+]);
